c82f5ed80329d89601246b60a13e31b860ddcd2a
dazia <abdelbarredazia@gmail.com>
2022-03-08 11:43:17 +0100

    front

--- frontend/src/Pages/Auth/Login.jsx
+++ frontend/src/Pages/Auth/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Login = () => {
 
-    <form>
+    return( <form>
         <div class="mb-6">
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
             <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required="" />
@@ -20,6 +20,7 @@ export const Login = () => {
             </div>
         </div>
         <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-    </form>
+    </form>);
+   
 
 }
